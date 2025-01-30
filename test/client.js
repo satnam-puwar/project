0d96fae2150efa15dedebc95a9866c2782e1dc81
Satnam Singh <satnampuwar.nugen@gmail.com>
2025-01-30 15:44:24 +0530

    ds

--- test/client.js
+++
@@ -1,9 +0,0 @@
-import { Client } from "@elastic/elasticsearch";
-
-const client = new Client({
-  node: "https://my-elasticsearch-project-fd3284.es.us-east-1.aws.elastic.cloud:443",
-  auth: {
-    apiKey: "ZHdhdVk1UUJQZ0VveWNwNWt1SlQ6amdpaWdmbzZRbzItZWxxbnZXaFpGdw==",
-  },
-});
-export { client };
